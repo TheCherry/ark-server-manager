02e476c2a48a4d8708975f91b70b564a2ab8cb1f
Daniel Kirschbaum <dk@lucom.eu>
2015-09-28 16:32:21 +0200

    src, dst imported. if(num == -1641380927) fix

--- src/z_unpack.py
+++ src/z_unpack.py
@@ -1,13 +1,14 @@
 
 def z_unpack(src, dst):
-    with open('PrimalItemVHBuggy.uasset.z', 'rb') as f:
-        compressedSize = f.read(8)
-        uncompressedSize = f.read(8)
-        compressedSize2 = f.read(8)
-        uncompressedSize2 = f.read(8)
-        num = uncompressedSize
-        if(num == -1641380927):
-            num = 131072L
+    with open(src, 'rb') as f:
+        with open(dst, 'wb') as f:
+            compressedSize = f.read(8)
+            uncompressedSize = f.read(8)
+            compressedSize2 = f.read(8)
+            uncompressedSize2 = f.read(8)
+            num = uncompressedSize
+            if(num == -1641380927):
+                num = 131072L
             num2 = (uncompressedSize2 + num - 1L) / num
             array = []
             num3 = 0L
@@ -15,12 +16,12 @@ def z_unpack(src, dst):
             while(num4 < num2):
                 data = f.read(16)
                 array[num4] = data[0-7]
-                num3 = Math.Max ( array[num4], num3)
+                num3 = Math.Max(array[num4], num3)
                 num4 += 1
                 num5 = 0L
                 while(num5 < num2):
                     compressed = array[num5]
                     array2 = f.read(compressed)
                     array3 = ZlibStream.UncompressBuffer(array2)
-                    binaryWriter.Write(array3)
+                    binaryWriter.Write(array3, dst)
                     num5 += 1L
