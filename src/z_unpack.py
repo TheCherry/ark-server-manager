31372038c6bb4a79f5c9b584eeb54c2fde2705e8
Daniel Kirschbaum <dk@lucom.eu>
2015-09-28 16:27:39 +0200

    copied unpack method from ARKRemote tool, thanks Face Wound. (Untested, imports missing, not runable)

---
+++ src/z_unpack.py
@@ -0,0 +1,26 @@
+
+def z_unpack(src, dst):
+    with open('PrimalItemVHBuggy.uasset.z', 'rb') as f:
+        compressedSize = f.read(8)
+        uncompressedSize = f.read(8)
+        compressedSize2 = f.read(8)
+        uncompressedSize2 = f.read(8)
+        num = uncompressedSize
+        if(num == -1641380927):
+            num = 131072L
+            num2 = (uncompressedSize2 + num - 1L) / num
+            array = []
+            num3 = 0L
+            num4 = 0
+            while(num4 < num2):
+                data = f.read(16)
+                array[num4] = data[0-7]
+                num3 = Math.Max ( array[num4], num3)
+                num4 += 1
+                num5 = 0L
+                while(num5 < num2):
+                    compressed = array[num5]
+                    array2 = f.read(compressed)
+                    array3 = ZlibStream.UncompressBuffer(array2)
+                    binaryWriter.Write(array3)
+                    num5 += 1L
