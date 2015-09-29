97c1eaa12b3fbda5e6afe83c19077142b30ea64b
Daniel Kirschbaum <dk@lucom.eu>
2015-09-29 10:20:52 +0200

    It's work!

--- src/z_unpack.py
+++ src/z_unpack.py
@@ -1,11 +1,11 @@
 import struct
 import zlib
 
-def print_strhex(st):
+def strhex(st):
     out = "0x"
     for char in st[::-1]:
         out += hex(ord(char))[2:4]
-    print out
+    return out
 
 
 def print_lhex(lo):
@@ -13,45 +13,38 @@ def print_lhex(lo):
 
 
 def str_to_l(st):
-    out = 0L
-    for char in st:
-        out += ord(char)
-    return out
-
-print_lhex(14124124L)
+    #out = 0L
+#    for char in st:
+#        out += ord(char)
+    #return out
+    return struct.unpack('L', st)[0]
 
 
 def z_unpack(src, dst):
     with open(src, 'rb') as f_src:
         with open(dst, 'wb') as f_dst:
-            f_src.read(8)
-            uncompressedSize = str_to_l(f_src.read(8))
+            t = f_src.read(8)
+            s = f_src.read(8)
+            uncompressedSize = str_to_l(s)
             f_src.read(8)
             uncompressedSize2 = str_to_l(f_src.read(8))
             num = uncompressedSize
             if(num == -1641380927):
                 num = 131072L
-            num2 = (uncompressedSize2 + num - 1L) / num
+            runs = (uncompressedSize2 + num - 1L) / num
             array = []
-            num3 = 0L
-            num4 = 0
-            print(num4)
-            print(num2)
-            while(num4 < num2):
+            i = 0
+            while(i < runs):
                 array.append(f_src.read(8))
                 f_src.read(8)
-                num3 = max(array[num4], num3)
-                num4 += 1
-                num5 = 0L
+                i += 1
+            i = 0
             data = ""
-            print("%i %i" % (num5, num2))
-            while(num5 < num2):
-                compressed = array[num5]
-                print(str_to_l(compressed))
+            while(i < runs):
+                compressed = array[i]
                 array2 = f_src.read(str_to_l(compressed))
                 data += array2
-                num5 += 1L
-            print_strhex(data)
+                i += 1
             array3 = zlib.decompress(data)
             f_dst.write(array3)
 
