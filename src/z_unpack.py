04b0f6ba9156d06dd933177bae459cd8b463e874
Daniel Kirschbaum <dk@lucom.eu>
2015-09-29 09:34:56 +0200

    First runs, with debug outputs. Dont work now, something is wrong in the second loop

--- src/z_unpack.py
+++ src/z_unpack.py
@@ -1,11 +1,33 @@
+import struct
+import zlib
+
+def print_strhex(st):
+    out = "0x"
+    for char in st[::-1]:
+        out += hex(ord(char))[2:4]
+    print out
+
+
+def print_lhex(lo):
+    print(hex(lo))
+
+
+def str_to_l(st):
+    out = 0L
+    for char in st:
+        out += ord(char)
+    return out
+
+print_lhex(14124124L)
+
 
 def z_unpack(src, dst):
     with open(src, 'rb') as f_src:
         with open(dst, 'wb') as f_dst:
-            compressedSize = f_src.read(8)
-            uncompressedSize = f_src.read(8)
-            compressedSize2 = f_src.read(8)
-            uncompressedSize2 = f_src.read(8)
+            f_src.read(8)
+            uncompressedSize = str_to_l(f_src.read(8))
+            f_src.read(8)
+            uncompressedSize2 = str_to_l(f_src.read(8))
             num = uncompressedSize
             if(num == -1641380927):
                 num = 131072L
@@ -13,15 +35,25 @@ def z_unpack(src, dst):
             array = []
             num3 = 0L
             num4 = 0
+            print(num4)
+            print(num2)
             while(num4 < num2):
-                data = f_src.read(16)
-                array[num4] = data[0-7]
-                num3 = Math.Max(array[num4], num3)
+                array.append(f_src.read(8))
+                f_src.read(8)
+                num3 = max(array[num4], num3)
                 num4 += 1
                 num5 = 0L
-                while(num5 < num2):
-                    compressed = array[num5]
-                    array2 = f_src.read(compressed)
-                    array3 = ZlibStream.UncompressBuffer(array2)
-                    binaryWriter.Write(array3, f_dst)
-                    num5 += 1L
+            data = ""
+            print("%i %i" % (num5, num2))
+            while(num5 < num2):
+                compressed = array[num5]
+                print(str_to_l(compressed))
+                array2 = f_src.read(str_to_l(compressed))
+                data += array2
+                num5 += 1L
+            print_strhex(data)
+            array3 = zlib.decompress(data)
+            f_dst.write(array3)
+
+z_unpack("/home/dk/steamcmd/steamapps/workshop/content/346110/525603940/LinuxNoEditor/VH_Buggy/Blueprint/PrimalItemVHBuggy.uasset.z",
+"/home/dk/steamcmd/steamapps/workshop/content/346110/525603940/LinuxNoEditor/VH_Buggy/Blueprint/PrimalItemVHBuggy.uasset")
